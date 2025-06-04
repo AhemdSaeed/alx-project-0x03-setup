79e7b778a11d062b8fb652f668f9cfdfa25bd935
Ahmed Saeed <ahmed.m.saeed120@gmail.com>
2025-06-04 14:51:57 +0300

    Init Proj

---
+++ alx-project-0x03/pages/_app.tsx
@@ -0,0 +1,14 @@
+ 
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <>
+  <Header/>
+
+  <Component {...pageProps} />;
+   <Footer/>
+  </>
+}
